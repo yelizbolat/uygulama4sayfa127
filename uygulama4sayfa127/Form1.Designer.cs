bf9ed16696c02062e9e1d54622fac97c91ad71b6
yeliz601 <yeliz601@BPC11>
2024-01-09 11:00:49 +0300

    Proje dosyası ekle.

---
+++ uygulama4sayfa127/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace uygulama4sayfa127
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAdSoyad = new System.Windows.Forms.Label();
+            this.btnDersNotu = new System.Windows.Forms.Label();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtDersNotu = new System.Windows.Forms.TextBox();
+            this.btnEnYuksek = new System.Windows.Forms.Button();
+            this.btnEnDusuk = new System.Windows.Forms.Button();
+            this.btnOrtalama = new System.Windows.Forms.Button();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.txtEnYuksek = new System.Windows.Forms.TextBox();
+            this.txtEnDusuk = new System.Windows.Forms.TextBox();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(37, 127);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(198, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "EKLE";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAdSoyad
+            // 
+            this.btnAdSoyad.AutoSize = true;
+            this.btnAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdSoyad.Location = new System.Drawing.Point(34, 54);
+            this.btnAdSoyad.Name = "btnAdSoyad";
+            this.btnAdSoyad.Size = new System.Drawing.Size(79, 16);
+            this.btnAdSoyad.TabIndex = 1;
+            this.btnAdSoyad.Text = "Ad Soyad:";
+            // 
+            // btnDersNotu
+            // 
+            this.btnDersNotu.AutoSize = true;
+            this.btnDersNotu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDersNotu.Location = new System.Drawing.Point(34, 96);
+            this.btnDersNotu.Name = "btnDersNotu";
+            this.btnDersNotu.Size = new System.Drawing.Size(80, 16);
+            this.btnDersNotu.TabIndex = 1;
+            this.btnDersNotu.Text = "Ders Notu:";
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Items.AddRange(new object[] {
+            " "});
+            this.lbListe.Location = new System.Drawing.Point(321, 50);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(101, 121);
+            this.lbListe.TabIndex = 2;
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(119, 50);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(147, 20);
+            this.txtAdSoyad.TabIndex = 3;
+            // 
+            // txtDersNotu
+            // 
+            this.txtDersNotu.Location = new System.Drawing.Point(119, 92);
+            this.txtDersNotu.Name = "txtDersNotu";
+            this.txtDersNotu.Size = new System.Drawing.Size(107, 20);
+            this.txtDersNotu.TabIndex = 4;
+            // 
+            // btnEnYuksek
+            // 
+            this.btnEnYuksek.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEnYuksek.Location = new System.Drawing.Point(37, 166);
+            this.btnEnYuksek.Name = "btnEnYuksek";
+            this.btnEnYuksek.Size = new System.Drawing.Size(91, 23);
+            this.btnEnYuksek.TabIndex = 0;
+            this.btnEnYuksek.Text = "En Yuksek";
+            this.btnEnYuksek.UseVisualStyleBackColor = true;
+            this.btnEnYuksek.Click += new System.EventHandler(this.btnEnYuksek_Click);
+            // 
+            // btnEnDusuk
+            // 
+            this.btnEnDusuk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEnDusuk.Location = new System.Drawing.Point(37, 195);
+            this.btnEnDusuk.Name = "btnEnDusuk";
+            this.btnEnDusuk.Size = new System.Drawing.Size(91, 23);
+            this.btnEnDusuk.TabIndex = 0;
+            this.btnEnDusuk.Text = "En Dusuk";
+            this.btnEnDusuk.UseVisualStyleBackColor = true;
+            this.btnEnDusuk.Click += new System.EventHandler(this.btnEnDusuk_Click);
+            // 
+            // btnOrtalama
+            // 
+            this.btnOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOrtalama.Location = new System.Drawing.Point(37, 224);
+            this.btnOrtalama.Name = "btnOrtalama";
+            this.btnOrtalama.Size = new System.Drawing.Size(91, 23);
+            this.btnOrtalama.TabIndex = 0;
+            this.btnOrtalama.Text = "Ortalama";
+            this.btnOrtalama.UseVisualStyleBackColor = true;
+            this.btnOrtalama.Click += new System.EventHandler(this.btnOrtalama_Click);
+            // 
+            // btnListe
+            // 
+            this.btnListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListe.Location = new System.Drawing.Point(321, 197);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(101, 23);
+            this.btnListe.TabIndex = 0;
+            this.btnListe.Text = "EKLE";
+            this.btnListe.UseVisualStyleBackColor = true;
+            this.btnListe.Click += new System.EventHandler(this.btnListe_Click);
+            // 
+            // txtEnYuksek
+            // 
+            this.txtEnYuksek.Location = new System.Drawing.Point(146, 168);
+            this.txtEnYuksek.Name = "txtEnYuksek";
+            this.txtEnYuksek.Size = new System.Drawing.Size(100, 20);
+            this.txtEnYuksek.TabIndex = 5;
+            // 
+            // txtEnDusuk
+            // 
+            this.txtEnDusuk.Location = new System.Drawing.Point(146, 197);
+            this.txtEnDusuk.Name = "txtEnDusuk";
+            this.txtEnDusuk.Size = new System.Drawing.Size(100, 20);
+            this.txtEnDusuk.TabIndex = 6;
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Location = new System.Drawing.Point(146, 226);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(100, 20);
+            this.txtOrtalama.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 295);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.txtEnDusuk);
+            this.Controls.Add(this.txtEnYuksek);
+            this.Controls.Add(this.txtDersNotu);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.btnDersNotu);
+            this.Controls.Add(this.btnAdSoyad);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.btnOrtalama);
+            this.Controls.Add(this.btnEnDusuk);
+            this.Controls.Add(this.btnEnYuksek);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Label btnAdSoyad;
+        private System.Windows.Forms.Label btnDersNotu;
+        private System.Windows.Forms.ListBox lbListe;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtDersNotu;
+        private System.Windows.Forms.Button btnEnYuksek;
+        private System.Windows.Forms.Button btnEnDusuk;
+        private System.Windows.Forms.Button btnOrtalama;
+        private System.Windows.Forms.Button btnListe;
+        private System.Windows.Forms.TextBox txtEnYuksek;
+        private System.Windows.Forms.TextBox txtEnDusuk;
+        private System.Windows.Forms.TextBox txtOrtalama;
+    }
+}
+
---
+++ uygulama4sayfa127/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama4sayfa127
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5]; 
+        int[] notlar = new int[5]; 
+        int index = 0;
+
+        private void btnEnYuksek_Click(object sender, EventArgs e)
+        {
+            int enyuksek = notlar[0];
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                if (notlar[i] > enyuksek)
+                {
+                    enyuksek = notlar[i];
+                }
+            }
+            txtEnYuksek.Text = enyuksek.ToString();
+        }
+
+        private void btnEnDusuk_Click(object sender, EventArgs e)
+        {
+            int endusuk = notlar[0];
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                if (notlar[i] < endusuk)
+                    endusuk = notlar[i];
+            }
+            txtEnDusuk.Text = endusuk.ToString();
+        }
+
+        private void btnOrtalama_Click(object sender, EventArgs e)
+        {
+            int toplam = 0;
+            double ortalama = 0;
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplam += notlar[i];
+            }
+            ortalama = toplam / notlar.Length;
+            txtOrtalama.Text = ortalama.ToString();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            isimler[index] = txtAdSoyad.Text;
+            notlar[index] = int.Parse(txtDersNotu.Text);
+            index++;
+            txtAdSoyad.Text = "";
+            txtDersNotu.Text = "";
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.Length; i++)
+            {
+                if (isimler[i] != null)
+                {
+                    lbListe.Items.Add(isimler[i] +" > "+notlar[i]);
+                }
+            }
+        }
+    }
+}
